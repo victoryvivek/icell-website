9a643d6539d0fe1fba58b2f3fec3551b5a007d3c
Shashwat-Sahu <shashwatsahu25@gmail.com>
2020-10-24 22:37:04 +0530

    Navbar and Landing edit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import { Route } from "react-router-dom";
 
 import Landing from "./components/Landing";
 import Home from "./components/Home";
-import Navbar from "./components/Navbar"
 import About from './components/About'
 
 class App extends Component {
-  render() {
 
+  render() {
+    
     return (
       <>
-        <Navbar />
-        <Route path="/home" exact component={Home} />
+      
         <Route path="/" exact component={Landing} />
+        <Route path="/home" exact component={Home} />
         <Route path="/about" exact component={About} />
       </>
     );
