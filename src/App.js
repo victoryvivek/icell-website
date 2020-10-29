07b501d337793bb76959990c0af746836979e5aa
victoryvivek <vivekaggarwalthegreat1998@gmail.com>
2020-10-29 17:03:02 +0530

    added events page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import { Route } from "react-router-dom";
 
 import Landing from "./components/Landing";
 import Home from "./components/Home";
-import About from './components/About'
+import About from "./components/About";
+import Events from "./components/Events";
 
 class App extends Component {
-
   render() {
-    
     return (
       <>
-      
         <Route path="/home" exact component={Home} />
         <Route path="/about" exact component={About} />
+        <Route path="/events" exact component={Events} />
         <Route path="/" exact component={Landing} />
       </>
     );
