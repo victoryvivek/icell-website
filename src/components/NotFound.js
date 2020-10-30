c21f299b5e90d9a2f8cf1d3078b7ce51f581b3f2
victoryvivek <vivekaggarwalthegreat1998@gmail.com>
2020-10-30 23:54:35 +0530

    added error page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 import Navbar from "../components/Navbar";
+import "../static/css/notfound.css";
 
 class NotFound extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        Not Found
+        <div id="notfound">
+          <div className="notfound">
+            <div className="notfound-404">
+              <h1>Oops!</h1>
+              <h2>404 - The Page can't be found</h2>
+            </div>
+            <Link to="/home">Go TO Homepage</Link>
+          </div>
+        </div>
       </div>
     );
   }
